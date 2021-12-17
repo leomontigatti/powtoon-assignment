a499d7ebd60e6a8831c4f7affeef12433d6124ea
Leo Monti <leomontigatti@gmail.com>
2021-12-17 00:08:08 -0300

    Modified modal props references, added rating stars, modified background image and text styles

--- src/CustomModal/index.js
+++ src/CustomModal/index.js
@@ -1,21 +1,46 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React from 'react'
+import { Modal } from 'react-bootstrap'
+import './CustomModal.css'
 
-function CustomModal (props) {
+function CustomModal ({ movie, show, closeModal }) {
+    
+    const movieBackdropPath = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
+    const movieReleaseYear = movie.release_date.slice(0, 4)
+    const movieCapitalizedTitle = movie.title.toUpperCase()
+    const movieRatingStars = Math.round(movie.vote_average) * 10
 
     return (
-        <Modal show={props.show} onHide={props.closeModal}>
-            <Modal.Header closeButton>
-                <Modal.Title>Heading Text</Modal.Title>
-            </Modal.Header>
-
-            <Modal.Body>{props.movieInfo.title}</Modal.Body>
-
-            <Modal.Footer>
-                <Button variant="secondary" onClick={props.closeModal}>Close</Button>
-            </Modal.Footer>
+        <Modal show={show} onHide={closeModal} size="lg" centered>
+            <Modal.Body style={{ 
+                backgroundImage: `url(${movieBackdropPath})`,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                filter: "blur(5px)",
+                WebkitFilter: "blur(5px)"
+            }}>
+            </Modal.Body>
+            <div className="modalText">
+                <p className="h2">{movieCapitalizedTitle}</p>
+                <div className="stars-outer">
+                    <div className="stars-inner" style={{
+                        width: movieRatingStars
+                    }}>
+                    </div>
+                </div>
+                <hr />
+                <div>
+                    <p>
+                        <span>
+                            <i className="fas fa-calendar-week"></i>
+                        </span> {movieReleaseYear}
+                    </p>
+                </div>  
+                <p>Original title: {movie.original_title}</p>
+                <p>{movie.overview}</p>
+            </div>
         </Modal>
-    );
-};
+    )
+}
 
-export { CustomModal };
+export { CustomModal }
